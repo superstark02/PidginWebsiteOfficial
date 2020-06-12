a77b0daa9d39fd0b7f12010d98631333dbc63696
Super Stark <superstark02@gmail.com>
2020-06-12 23:24:53 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hell0</h1>
+      
+
+      <div class="pimg2">
+        <div class="ptext">
+          <span className="border trans" style={{backgroundColor:'rgba(255, 255, 255, 0.5)'}}>
+            SEARCH INPUT
+        </span>
+        </div>
+      </div>
+
+      <section className="section section-dark">
+        <h2>ABOUT US</h2>
+        <p>
+          We are here to provide you the best experience of learning. 
+          It may be music, cooking or school examinations. We provide all the detials of the classes around you on your phone.
+          Just browse through hundreds of classes, choose and get enrolled.
+        </p>
+      </section>
+
+      <div className="pimg3">
+        <div className="ptext">
+          <span className="border trans" style={{color:'black'}} >
+            CATEGORIES HERE
+      </span>
+        </div>
+      </div>
+
+      <section class="section section-dark">
+        <h2>LIST HERE</h2>
+        <p>
+          List Items.
+        </p>
+      </section>
+
+      <div class="pimg1">
+        <div class="ptext">
+          <span class="border">
+            GET THE APP
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
